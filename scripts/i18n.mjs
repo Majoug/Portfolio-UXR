e76758475b3499e1203bd7e83171e3bb3b1fcedc
JMDidon <jeanmichel.didon@gmail.com>
2025-05-03 16:54:05 +0200

    feat(home): Implement i18n. #3

---
+++ scripts/i18n.mjs
@@ -0,0 +1,78 @@
+import { Signal } from './signal.mjs';
+
+export class I18n {
+    static #instance = null;
+    static #supportedLocales = ['fr', 'en', 'es'];
+    static #LOCALE_STORAGE_KEY = 'userPreferredLocale';
+
+    #readLocale;
+    #writeLocale;
+    #readTranslations;
+    #writeTranslations;
+
+    constructor() {
+        if (I18n.#instance) {
+            return I18n.#instance;
+        }
+
+        const initialLocale = this.#determineInitialLocale();
+        [this.#readLocale, this.#writeLocale] = Signal.sg(initialLocale);
+        [this.#readTranslations, this.#writeTranslations] = Signal.sg({});
+
+        this.#loadTranslations(this.#readLocale());
+        I18n.#instance = this;
+    }
+
+    t(key) {
+        const translations = this.#readTranslations();
+        const value = key.split('.').reduce((obj, prop) => {
+            return obj ? obj[prop] : null;
+        }, translations);
+        
+        return value;
+    }
+
+    get currentLocale() {
+        return this.#readLocale();
+    }
+
+    async setLocale(newLocale) {
+        this.#writeLocale(newLocale);
+        this.#setStoredLocale(newLocale);
+        await this.#loadTranslations(newLocale);
+    }
+
+    #getStoredLocale() {
+        return localStorage.getItem(I18n.#LOCALE_STORAGE_KEY);
+    }
+
+    #setStoredLocale(locale) {
+        localStorage.setItem(I18n.#LOCALE_STORAGE_KEY, locale);
+    }
+
+    #determineInitialLocale() {
+        const stored = this.#getStoredLocale();
+        if (stored && I18n.#supportedLocales.includes(stored)) {
+            return stored;
+        }
+
+        const userLocales = navigator.languages;
+        const foundLocale = userLocales.find(locale => {
+            const baseLocale = locale.split('-')[0].toLowerCase();
+            return I18n.#supportedLocales.includes(baseLocale);
+        });
+
+        if (foundLocale) {
+            return foundLocale.split('-')[0].toLowerCase();
+        }
+
+        return 'en';
+    }
+
+    async #loadTranslations(locale) {
+        const translationPath = `./public/i18n/${locale}.json`;
+        const response = await fetch(translationPath);
+        const loadedTranslations = await response.json();
+        this.#writeTranslations(loadedTranslations);
+    }
+}
---
+++ scripts/language-select.mjs
@@ -0,0 +1,17 @@
+import { I18n } from './i18n.mjs';
+
+function initializeLanguageSelector() {
+    const selectElement = document.getElementById('language-select');
+    
+    const i18n = new I18n();
+    selectElement.value = i18n.currentLocale;
+    selectElement.addEventListener('change', (event) => {
+        i18n.setLocale(event.target.value);
+    });
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initializeLanguageSelector);
+} else {
+    initializeLanguageSelector();
+} 
